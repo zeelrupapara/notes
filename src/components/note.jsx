3fdc1ef920784efb43bcfd37f4043769f0ac5743
zeelrupapara <zeelrupapara@gmail.com>
2022-09-29 14:25:22 +0530

    feat: change col-lg-4 to col-lg-3 for note component

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,6 +1,6 @@
 export function Note() {
     return (
-        <div id="note-box" className="col col-sm-4 shadow rounded m-1 mb-4">
+        <div id="note-box" className="col col-lg-3 shadow rounded m-1 mb-4">
             <div className="my-3 container text-left">
                 <h5 className="font-weight-bold">#Jeel's Note</h5>
                 <h6 className="text-muted font-weight-light font-italic">
